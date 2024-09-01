150bad48e56681b9427dff70c025838489375d3c
Mythical-Github <MythicalData@gmail.com>
2024-09-01 02:55:55 -0400

    fixed bug with spaces in paths

--- gen_py_utils/gen_py_utils.py
+++ gen_py_utils/gen_py_utils.py
@@ -23,11 +23,11 @@ def download_file(url: str, download_path: str):
 
 
 def open_dir_in_file_browser(input_directory: str):
-    os.system(f'explorer {input_directory}')
+    os.system(f'explorer "{input_directory}"')
 
 
 def open_file_in_default(file_path: str):
-    os.system(f'start {file_path}')
+    os.system(f'start "{file_path}"')
 
 
 def open_website(input_url: str):
